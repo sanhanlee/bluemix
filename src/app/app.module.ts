ba961c14ff0304a739c05c49e160731434424f39
sanket bhalerao <sanket_bhalerao@persistent.co.in>
2017-06-14 19:57:58 +0530

    changes in .travis.yml and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {Routes,RouterModule} from '@angular/router'
-import {HttpModule,JsonpModule} from '@angular/http'
+import { Routes, RouterModule } from '@angular/router';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
-import { ChatComponent } from './custom/chat.component'
+import { ChatComponent } from './custom/chat.component';
 
 const routes:Routes=[
   {
-    path:'',
-    component:ChatComponent
+    path: '',
+    component: ChatComponent
   }
 ];
 
 @NgModule({
-  imports:      [ BrowserModule,RouterModule.forRoot(routes),HttpModule,JsonpModule ],
-  declarations: [ AppComponent,ChatComponent ],
+  imports:      [ BrowserModule, RouterModule.forRoot(routes), HttpModule, JsonpModule ],
+  declarations: [ AppComponent, ChatComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
